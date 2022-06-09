dbf7ebf43e05dc46116fb43e8006b732269ffb2e
Zineddine A <zineddine.arar@gmail.com>
2022-06-09 15:38:00 +0200

    Models Class

---
+++ app/src/main/java/com/example/myapplication/models.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication
+
+data class AuthToken(
+    val username : String,
+    val password : String,
+    val token : String
+)
+
+data class Artist(
+    val id: Int,
+    val name: String,
+    val genre_name: String,
+)
+
+data class Genre(
+    val id :Int,
+    val name : String,
+    val created_at: String
+)
+
+data class Song(
+    val id : Int,
+    val name: String,
+    val file_url:String,
+    val duration:Int,
+    val created_at: String,
+    val artist: Int
+)
+
