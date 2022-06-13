38d58cdfc46a84eede9f46e7c46a7fbd43be4368
Zineddine A <zineddine.arar@gmail.com>
2022-06-13 16:15:10 +0200

    Media Player set to Global instead of Local

--- app/src/main/java/com/example/myapplication/audioPlayer/AudioPlayerFragment.kt
+++ app/src/main/java/com/example/myapplication/audioPlayer/AudioPlayerFragment.kt
@@ -13,7 +13,9 @@ import androidx.navigation.fragment.navArgs
 import com.example.myapplication.R
 import com.example.myapplication.Song
 import com.example.myapplication.api.APIArtist
+import com.example.myapplication.api.globalApiKey
 import com.example.myapplication.databinding.FragmentAudioPlayerBinding
+import com.example.myapplication.globalMediaPlayer
 import com.squareup.picasso.Picasso
 import java.util.concurrent.TimeUnit
 import kotlin.properties.Delegates
@@ -25,7 +27,6 @@ class AudioPlayerFragment : Fragment() {
     lateinit var Songs:List<Song>
     var CurrentIndex by Delegates.notNull<Int>()
     private lateinit var binding: FragmentAudioPlayerBinding
-    lateinit var mediaPlayer:MediaPlayer
     private val args : AudioPlayerFragmentArgs by navArgs()
     lateinit var runnable: Runnable
 
@@ -45,13 +46,14 @@ class AudioPlayerFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
-        mediaPlayer.pause() // can change later
+        //globalMediaPlayer.pause() // can change later
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         var handler: Handler = Handler()
+
 
 
         song = args.song
@@ -62,7 +64,6 @@ class AudioPlayerFragment : Fragment() {
         binding.TotalTimeView.text = getDisplayedTime(song.duration)
         binding.SongTitleView.text=song.name
         binding.SongTitleView.isSelected
-        mediaPlayer = MediaPlayer()
         Picasso.get().load(album.album_cover_url)
             .into(binding.imageView3)
         binding.PausePlayButtonView.setOnClickListener(){
@@ -80,8 +81,8 @@ class AudioPlayerFragment : Fragment() {
         // Change seekbar position whenever mediaplayer's position changes
         binding.seekBarView.setOnSeekBarChangeListener(object :SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, p1: Int, fromUser: Boolean) {
-                if(fromUser && mediaPlayer!=null){
-                    mediaPlayer.seekTo(p1*1000)
+                if(fromUser && globalMediaPlayer!=null){
+                    globalMediaPlayer.seekTo(p1*1000)
                 }
             }
 
@@ -98,10 +99,10 @@ class AudioPlayerFragment : Fragment() {
 
         runnable = Runnable {
             run(){
-                if(mediaPlayer!=null){
-                    binding.seekBarView.progress = mediaPlayer.currentPosition/1000
-                    binding.CurrentTimeView.text=convertToMMSS("${mediaPlayer.currentPosition}")
-                    if(mediaPlayer.isPlaying){
+                if(globalMediaPlayer!=null){
+                    binding.seekBarView.progress = globalMediaPlayer.currentPosition/1000
+                    binding.CurrentTimeView.text=convertToMMSS("${globalMediaPlayer.currentPosition}")
+                    if(globalMediaPlayer.isPlaying){
                         binding.PausePlayButtonView.setImageResource(R.drawable.ic_baseline_pause_circle_outline_24)
                     }else{
                         binding.PausePlayButtonView.setImageResource(R.drawable.ic_baseline_play_circle_outline_24)
@@ -117,22 +118,22 @@ class AudioPlayerFragment : Fragment() {
     }
 
     private fun playMusic() {
-        mediaPlayer.reset()
-        mediaPlayer.setDataSource(song.file_url)
-        mediaPlayer.prepare()
+        globalMediaPlayer.reset()
+        globalMediaPlayer.setDataSource(song.file_url)
+        globalMediaPlayer.prepare()
         binding.seekBarView.progress = 0
         binding.seekBarView.max=song.duration
 
-        mediaPlayer.start()
+        globalMediaPlayer.start()
     }
 
 
 
     private fun pausePlayMusic(){
-        if(mediaPlayer.isPlaying){
-            mediaPlayer.pause()
+        if(globalMediaPlayer.isPlaying){
+            globalMediaPlayer.pause()
         }else{
-            mediaPlayer.start()
+            globalMediaPlayer.start()
         }
     }
 
--- app/src/main/java/com/example/myapplication/fragment_home.kt
+++ app/src/main/java/com/example/myapplication/fragment_home.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -11,12 +12,14 @@ import androidx.fragment.app.viewModels
 import com.example.myapplication.databinding.FragmentHomeBinding
 import com.example.myapplication.login.LoginViewModel
 import androidx.navigation.fragment.findNavController
+var globalMediaPlayer: MediaPlayer = MediaPlayer()
 
 class HomeFragment : Fragment() {
     val viewModel : LoginViewModel by viewModels()
 
     //Unify Media Player With This code in every fragment page
 
+    var globalSomething : String = ""
 
 
     lateinit var binding: FragmentHomeBinding
